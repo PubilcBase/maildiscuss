46a02e711726f3b63a0c546ecc942ed4e850b740
Andrew T <16790483+ateesdalejr@users.noreply.github.com>
2017-12-01 14:23:38 -0800

    added basic router framework

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
-echo "You've requested: ".$_GET["route"];
+
+class Router {
+    function __construct() {
+        $this->routes = array();
+        $this->routes_keys = array();
+    }
+    public function add_route($route, $route_callback) {
+        $this->routes[$route] = $route_callback;
+        array_push($this->routes_keys, $route);
+    }
+    public function route($requested_route) {
+        $requested_route="/".$requested_route;
+        if (in_array($requested_route, $this->routes_keys)) {
+            $this->routes[$requested_route]();
+        } else {
+            $this->route("404");
+        }
+    }
+}
+$router = new Router();
+$router->add_route("/404", function () {
+    echo("Oops! That looks like a 404!");
+});
+$router->add_route("/hey", function () {
+    echo("Welcome to the index of the page!");
+});
+$router->route($_GET["route"]);
 ?>
